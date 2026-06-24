6a253051146f15ce2f5dc887a819389067fd31cc
Lukas Ketzer <lukas.a.ketzer@tum.de>
2026-06-24 10:36:22 +0200

    feat: hierarchical classification evaluator and confusion-matrix refactor
    Adds HierarchicalClassificationEvaluator and HierarchicalMetricResults for
    per-hop precision/recall/F1 reporting on ancestor-descendant tasks.
    Refactors ConfusionMatrixClassificationMetric.extract_from_confusion_matrix
    to accept flat (tn, fp, fn, tp) args instead of a matrix, following
    sklearn's ravel() convention.

--- src/pykeen/evaluation/__init__.py
+++ src/pykeen/evaluation/__init__.py
@@ -5,6 +5,10 @@ from class_resolver import ClassResolver
 from .classification_evaluator import ClassificationEvaluator, ClassificationMetricResults
 from .evaluation_loop import LCWAEvaluationLoop
 from .evaluator import Evaluator, MetricResults
+from .hierarchical_classification_evaluator import (
+    HierarchicalClassificationEvaluator,
+    HierarchicalMetricResults,
+)
 from .ogb_evaluator import OGBEvaluator
 from .rank_based_evaluator import (
     MacroRankBasedEvaluator,
@@ -24,6 +28,8 @@ __all__ = [
     "OGBEvaluator",
     "ClassificationEvaluator",
     "ClassificationMetricResults",
+    "HierarchicalClassificationEvaluator",
+    "HierarchicalMetricResults",
     "evaluator_resolver",
     "metric_resolver",
 ]
